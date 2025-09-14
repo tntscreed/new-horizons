345420db3044da682a9eeea95c7f58dd9146fdd7
tntscreed <126970474+tntscreed@users.noreply.github.com>
2025-09-14 14:39:14 +0200

    LayerDelObject string to constant fixes (by Mirsaneli)

--- PROGRAM/Weather/WhrWeather.c
+++ PROGRAM/Weather/WhrWeather.c
@@ -1350,8 +1350,8 @@ void DeleteWeather()
 
 void MoveWeatherToLayers(string sExecuteLayer, string sRealizeLayer)
 {
-	LayerDelObject("execute",&Weather);
-	LayerDelObject("realize",&Weather);
+	LayerDelObject(EXECUTE,&Weather);
+	LayerDelObject(REALIZE,&Weather);
 	LayerDelObject(SEA_EXECUTE,&Weather);
 	LayerDelObject(SEA_REALIZE,&Weather);
 
